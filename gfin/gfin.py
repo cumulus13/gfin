3f9a94efa80a42bf06577c921a702823eb68a184
cumulus13 <cumulus13@gmail.com>
2025-05-27 19:30:51 +0700

    version: 0.13 ~ 2025-05-27 19:30:48:381174

--- gfin/gfin.py
+++ gfin/gfin.py
@@ -15,26 +15,49 @@ except:
 		return ''
 # from makelist import makeList
 
+def get_data_currencies(content):
+    # html = bs_obj.prettify()
+    # Pola untuk menangkap kode dan nama mata uang
+    pattern = re.compile(
+        r'<a href="/currency/([a-z]{3})-[^/]+/" class="[^"]*?">\s*<div[^>]*?>.*?</div>\s*<span class="currencyCode">([A-Z]{3})<!-- --> -</span>\s*<!-- -->\s*(.*?)</a>',
+        re.DOTALL
+    )
+
+    currencies = []
+    for match in pattern.finditer(content):
+        slug_code = match.group(1).upper()
+        code = match.group(2).upper()
+        name = match.group(3).strip()
+        currencies.append({
+            "code": code,
+            "name": name,
+            "slug_code": slug_code,
+        })
+
+    return currencies
+
 def currency():
 	content = requests.get("https://www.xe.com/iso4217.php").content
-	b = bs(content, 'html.parser')
-	all_currentcy = b.find_all('td', {'class':"tblBrdrLn"})
-	debug(all_currentcy = all_currentcy)
-	data = {}
-	# table = Texttable()
-	# table.header(['CODE','NAME'])
-	# table.set_cols_align(["c", "c"])
-	# table.set_cols_width([10, 50])
-	for i in all_currentcy:
-		# debug(i = i)
-		if i.find('a'):
-			key = i.find('a').text.strip()
-			value = all_currentcy[all_currentcy.index(i) + 1].text
-			data.update({key:value})
-			# table.add_row([key, value])
-	# print(table.draw())
+	# b = bs(content, 'html.parser')
+	data = get_data_currencies(content.decode())
 	debug(data = data)
-	# makeList([[i, data.get(i)] for i in data], 2)
+	# all_currentcy = b.find_all('td', {'class':"tblBrdrLn"})
+	# debug(all_currentcy = all_currentcy)
+	# data = {}
+	# # table = Texttable()
+	# # table.header(['CODE','NAME'])
+	# # table.set_cols_align(["c", "c"])
+	# # table.set_cols_width([10, 50])
+	# for i in all_currentcy:
+	# 	# debug(i = i)
+	# 	if i.find('a'):
+	# 		key = i.find('a').text.strip()
+	# 		value = all_currentcy[all_currentcy.index(i) + 1].text
+	# 		data.update({key:value})
+	# 		# table.add_row([key, value])
+	# # print(table.draw())
+	# debug(data = data)
+	# # makeList([[i, data.get(i)] for i in data], 2)
 	return data
 
 def convert(fr, to, data):
@@ -69,28 +92,37 @@ def convert(fr, to, data):
 
 def usage():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('FROM', action = 'store', help = 'Convert From (code)')
-	parser.add_argument('TO', action = 'store', help = 'Convert To (code)')
-	parser.add_argument('NUMBER', action = 'store', help = 'Data Number to Convert to')
+	parser.add_argument('FROM', action = 'store', help = 'Convert From (code)', nargs='?')
+	parser.add_argument('TO', action = 'store', help = 'Convert To (code)', nargs='?')
+	parser.add_argument('NUMBER', action = 'store', help = 'Data Number to Convert to', nargs='?')
 	parser.add_argument('-l', '--list', action = 'store_true', help = 'List currency Code')
+	parser.add_argument('-f', '--find', action = 'store', help = 'Find currency Code')
 	if len(sys.argv) == 1:
 		parser.print_help()
 	else:
-		if len(sys.argv) == 2:
-			if sys.argv[1] == '-l' or sys.argv[1] == '--list':
-				data = currency()
-				for i in data.keys():
-					print(make_colors(i, 'lc') + " --> " + make_colors(data.get(i), 'ly'))
-		else:
-			args = parser.parse_args()
-			if args.list:
-				data = currency()
-				for i in data.keys():
-					print(make_colors(i, 'lc') + " --> " + make_colors(data.get(i), 'ly'))
-			
+		args = parser.parse_args()
+		if args.list:
+			data = currency()
+			for i in data:
+				print(make_colors(i.get('code'), 'lc') + " --> " + make_colors(i.get('name'), 'ly'))
+		elif args.find:
+			data = currency()
+			found = []
+			for i in data:
+				if args.find.lower() in i.get('name').lower() or args.find.lower() in i.get('code').lower():
+					found.append(i)
+			if not found:
+				print(make_colors("NOT FOUND:", 'lw', 'r') + " " + make_colors(args.find, 'lc'))
+				sys.exit(1)
+			debug(found = found)
+			unique = list({tuple(d.items()) for d in found})
+			result = [dict(t) for t in unique]
+			debug(result = result)
+			for i in result:
+				print(make_colors(i.get('code'), 'lc') + " --> " + make_colors(i.get('name'), 'ly'))
+		elif args.FROM and args.TO and args.NUMBER:
 			data, base = convert(args.FROM, args.TO, args.NUMBER)
 			print(make_colors(args.FROM.upper(), 'ly') + " [" + make_colors(base, 'lm') + "] " + make_colors('-->', 'lg') + " " + make_colors(args.TO.upper(), 'bl') + " :" + make_colors(args.TO.upper() + ". " + data, 'lw', 'r'))
-
 
 if __name__ == '__main__':
 	usage()
